20d18ff1f7a9c92ad18e79b76c9eecc8b2c4a211
Billy Lai <zhanlai12@gmail.com>
2020-10-16 19:25:19 -0500

    fix time format

--- src/app/time-converter.ts
+++ src/app/time-converter.ts
@@ -15,7 +15,16 @@ export class TimeConverter {
     return (this._time % 60000) / 1000;
   }
 
+  fullFormat(): string {
+    let secondSplit = this.second.toString().split('.');
+    let secondStr = secondSplit[0].padStart(2, '0');
+    let millisecondStr = secondSplit.length < 2 ? "000" : secondSplit[1].padEnd(3, '0');
+    return `${this.minute.toString().padStart(2, '0')}:${secondStr}.${millisecondStr}`;
+  }
+
   format(): string {
-    return `${this.minute.toString().padStart(2, '0')}:${this.second.toString().padStart(2, '0')}`;
+    let secondSplit = this.second.toString().split('.');
+    let secondStr = secondSplit[0].padStart(2, '0');
+    return `${this.minute.toString().padStart(2, '0')}:${Math.floor(this.second).toString().padStart(2, '0')}`;
   }
 }
